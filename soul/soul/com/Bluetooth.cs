d2fc2470e49afe2699368a1ab0ec479e9e7a3f3b
seojunui <tjwnsdml@nate.com>
2013-11-30 02:38:57 +0900

    pcApplication추가

---
+++ soul/soul/com/Bluetooth.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using soul.data;
+
+namespace soul.com
+{
+    class Bluetooth : DataManager
+    {
+        SerialPort serial;
+        string g_sRecvData = String.Empty;
+
+        uti.KeyboardControl kbc = new uti.KeyboardControl();
+
+        public Bluetooth()
+        {
+            serial = new SerialPort();
+            //DataC = new DataManager();
+        }
+        
+        public void InitSerialPort(string PortName)
+        {
+            Console.WriteLine("open");
+            serial.PortName = PortName;
+            serial.BaudRate = 115200;
+            serial.DataBits = 8;
+            serial.Parity = Parity.None;
+            serial.StopBits = StopBits.One;
+            serial.ReadTimeout = 10000;
+            serial.WriteTimeout = 10000;           
+        }
+        public void Start(){
+            serial.Open();
+            serial.DataReceived += new SerialDataReceivedEventHandler(serial_DataReceived);
+        }
+        
+        private void serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string readData = serial.ReadExisting();
+
+                
+                if ((readData != string.Empty))
+                {
+                    SetData(readData);
+                    //DataC.SetData(readData);  
+                    kbc.Start();
+                }
+            }
+            catch (TimeoutException)
+            {
+                g_sRecvData = string.Empty;
+            }
+        }
+
+
+
+    }
+}
---
+++ soul/soul/uti/Tray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace soul.uti
+{
+    class Tray
+    {
+        private System.Windows.Threading.DispatcherTimer trayToolTiptimer;
+        private System.Windows.Forms.NotifyIcon notify;
+        private System.Windows.Window window;
+        public Tray()
+        {
+            notify = new System.Windows.Forms.NotifyIcon();
+            notify.Icon = soul.Properties.Resources.trayicon;
+        }
+
+        public void setWindow(System.Windows.Window _window)
+        {
+            this.window = _window;
+        }
+
+        public void Start()
+        {
+            try
+            {                
+                notify.Visible = true;
+
+                notify.DoubleClick +=
+                    delegate(object senders, EventArgs args)
+                    {
+                        //this.Show();
+                        //this.WindowState = WindowState.Normal;
+                        //this.Close();
+                        this.window.Close();
+                    };
+
+                trayToolTiptimer = new System.Windows.Threading.DispatcherTimer();
+                trayToolTiptimer.Interval = new TimeSpan(0, 0, 1);
+                trayToolTiptimer.Tick += new EventHandler(trayToolTiptimer_Tick);
+                trayToolTiptimer.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("tray Exeption");
+                trayToolTiptimer.Stop();
+            }
+        }
+
+        private void trayToolTiptimer_Tick(object sender, EventArgs e)
+        {
+            notify.BalloonTipTitle = "S.M.S";
+            notify.BalloonTipText = "Window Control start";
+            notify.ShowBalloonTip(500);
+
+            trayToolTiptimer.Stop();
+        }
+
+    }
+}
